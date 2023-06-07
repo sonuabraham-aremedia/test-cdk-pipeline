a8a886ab0fb600332c1b55525ac4187f893e4be3
Sonu Abraham <sonu.abraham@aremedia.com.au>
2023-06-07 16:56:15 +1000

    initial commit

---
+++ bin/my-pipeline.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '021007710463',
+    region: 'ap-southeast-2',
+  }
+});
+
+app.synth();
